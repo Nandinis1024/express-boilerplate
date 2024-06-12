685221738b0c1a2821718621d6d9880326f54f5f
Nandinis1024 <nandini.sunil1024@gmail.com>
2024-06-12 14:12:16 +0530

    xeno backend complete

--- User.js
+++ User.js
@@ -12,8 +12,21 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     trim: true,
-  }
-});
+  },
+  totalSpents: {
+    type: Number,
+    default: 0,
+  },
+  lastVisit: {
+    type: Date,
+    default: Date.now,
+  },
+  totalVisits: {
+    type: Number,
+    default: 0,
+  },
+}, { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
